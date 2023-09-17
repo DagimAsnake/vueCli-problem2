15c6678f5b1b98ba5401f37037351d93d1fa7ca1
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-17 02:36:41 -1200

    finished the problem

---
+++ src/components/LearningResource/TheResource.vue
@@ -0,0 +1,86 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import AddResource from "./AddResource.vue";
+import StoredResources from "./StoredResources.vue";
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === "stored-resources" ? null : "flat";
+    },
+    addResButtonMode() {
+      return this.selectedTab === "add-resource" ? null : "flat";
+    },
+  },
+  data() {
+    return {
+      selectedTab: "stored-resources",
+      storedResource: [
+        {
+          id: "official-guide",
+          title: "Offical Guide",
+          description: "The official vue.js documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google.....",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResource.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    },
+     removeResource(resId) {
+      const resIndex = this.storedResource.findIndex(res => res.id === resId);
+      this.storedResource.splice(resIndex, 1);
+    },
+  },
+  provide() {
+    return {
+      resource: this.storedResource,
+      addResource: this.addResource,
+      deleteResource: this.removeResource
+    };
+  },
+};
+</script>
+    BaseButton
+
+<style>
+</style>
